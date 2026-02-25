e2608609449a9a22682257436bae2fac4ed31143
kokatsu <66732063+kokatsu@users.noreply.github.com>
2026-02-25 21:41:31 +0900

    feat(Neovim): add auto port detection for swagger-preview

--- .config/nvim/lua/plugins/swagger-preview.lua
+++ .config/nvim/lua/plugins/swagger-preview.lua
@@ -4,5 +4,18 @@ return {
   'vinnymeller/swagger-preview.nvim',
   cmd = { 'SwaggerPreview', 'SwaggerPreviewStop', 'SwaggerPreviewToggle' },
   build = 'npm i',
-  config = true,
+  opts = function()
+    local port = 8823 -- https://open.spotify.com/intl-ja/track/3Un4DTzaw3l3BH31tLdrKl
+    for i = 0, 9 do
+      local s = vim.uv.new_tcp()
+      if s then
+        local ok = pcall(s.bind, s, '127.0.0.1', port + i)
+        s:close()
+        if ok then
+          return { port = port + i, host = 'localhost' }
+        end
+      end
+    end
+    return { port = port, host = 'localhost' }
+  end,
 }
