859e0a97fcc97a137b440b5178288edd93bf85dc
kokatsu <66732063+kokatsu@users.noreply.github.com>
2026-02-27 01:38:38 +0900

    style(shell): apply shfmt formatting and shellcheck fixes

--- .config/tmux/scripts/octorus-history.sh
+++ .config/tmux/scripts/octorus-history.sh
@@ -4,7 +4,7 @@
 
 RALLY_DIR="$HOME/.cache/octorus/rally"
 
-[[ ! -d "$RALLY_DIR" ]] && echo "No octorus rally data found" && read && exit 1
+[[ ! -d "$RALLY_DIR" ]] && echo "No octorus rally data found" && read -r && exit 1
 
 # 現在のリポジトリに絞り込む (owner_repo プレフィックスでフィルタ)
 # display-popup -d でペインの作業ディレクトリが pwd に設定される
@@ -19,12 +19,12 @@ if [[ -n "$repo_prefix" ]]; then
   files=$(echo "$files" | grep -F "/$repo_prefix")
 fi
 
-[[ -z "$files" ]] && echo "No rally data found for this repository" && read && exit 0
+[[ -z "$files" ]] && echo "No rally data found for this repository" && read -r && exit 0
 
 selected=$(echo "$files" |
   fzf --preview 'bat --color=always --language=json {}' \
-      --preview-window=right:60% \
-      --delimiter '/' \
-      --with-nth -3,-2,-1)
+    --preview-window=right:60% \
+    --delimiter '/' \
+    --with-nth -3,-2,-1)
 
 [[ -n "$selected" ]] && tmux send-keys -l "$selected"
