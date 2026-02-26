2fef4f2c1f146603862f27674602fc0afc4849a2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2026-02-27 01:42:54 +0900

    style: fix EditorConfig violations

--- .config/zsh/darwin.zsh
+++ .config/zsh/darwin.zsh
@@ -28,7 +28,7 @@ bindkey -M viins '^W' backward-kill-word
 # ------------------------------------------------------------------------------
 
 if [[ -n "$GHOSTTY_RESOURCES_DIR" ]]; then
-    source "$GHOSTTY_RESOURCES_DIR/shell-integration/zsh/ghostty-integration"
+  source "$GHOSTTY_RESOURCES_DIR/shell-integration/zsh/ghostty-integration"
 fi
 
 # ------------------------------------------------------------------------------
