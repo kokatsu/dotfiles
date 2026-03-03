5b25abf9d0b62d022f4b944806e0e1e1b2ac82b1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2026-03-03 22:35:24 +0900

    fix(nvim): migrate colorizer to catgoose/nvim-colorizer.lua
    Switch from archived norcalli/nvim-colorizer.lua to actively maintained
    catgoose fork with updated configuration API.

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,24 +1,25 @@
 return {
-  'norcalli/nvim-colorizer.lua',
+  'catgoose/nvim-colorizer.lua',
   event = { 'BufReadPre', 'BufNewFile' },
   config = function()
     require('colorizer').setup({
-      'vue',
-      'css',
-      'scss',
-      'html',
-      'javascript',
-      'typescript',
-      'typescriptreact',
-      'javascriptreact',
-      'lua',
-    }, {
-      RGB = true,
-      RRGGBB = true,
-      names = false,
-      RRGGBBAA = true,
-      css = true,
-      css_fn = true,
+      filetypes = {
+        'vue',
+        'css',
+        'scss',
+        'html',
+        'javascript',
+        'typescript',
+        'typescriptreact',
+        'javascriptreact',
+        'lua',
+      },
+      options = {
+        parsers = {
+          css = true,
+          names = { enable = false },
+        },
+      },
     })
   end,
 }
