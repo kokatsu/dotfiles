5d95e4312c2d46ffcbbb4d9b4cfe5268d4683be4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2026-03-17 22:15:32 +0900

    feat(nvim): enable vue hybrid mode with typescript-tools
    Switch to kokatsu/typescript-tools.nvim fork with tsserver plugin
    support. Enable hybrid mode so vue_ls handles templates/CSS while
    typescript-tools handles TypeScript via @vue/typescript-plugin.
    Add Nix store path detection for tsserver and vue plugin binaries.

--- .config/nvim/lua/plugins/typescript-tools.lua
+++ .config/nvim/lua/plugins/typescript-tools.lua
@@ -1,13 +1,50 @@
--- https://github.com/pmizio/typescript-tools.nvim
+-- https://github.com/kokatsu/typescript-tools.nvim
+
+-- Nix 環境: バイナリの実パスから Nix ストアプレフィックスを取得
+local function nix_store_prefix(bin_name)
+  local bin = vim.fn.exepath(bin_name)
+  if bin == '' then
+    return nil
+  end
+  return vim.fn.resolve(bin):match('(/nix/store/[^/]+)')
+end
+
+-- vtsls にバンドルされた tsserver.js を検出
+local function find_tsserver_path()
+  local prefix = nix_store_prefix('vtsls')
+  if not prefix then
+    return nil
+  end
+  local found = vim.fn.glob(prefix .. '/lib/*/node_modules/.pnpm/typescript@*/node_modules/typescript/lib/tsserver.js')
+  if found ~= '' then
+    return found:match('[^\n]+')
+  end
+  return nil
+end
+
+-- vue-language-server にバンドルされた @vue/typescript-plugin を検出
+local function find_vue_typescript_plugin()
+  local prefix = nix_store_prefix('vue-language-server')
+  if not prefix then
+    return nil
+  end
+  local found = vim.fn.glob(prefix .. '/lib/*/node_modules/.pnpm/node_modules/@vue/typescript-plugin')
+  if found ~= '' then
+    return found:match('[^\n]+')
+  end
+  return nil
+end
 
 return {
-  'pmizio/typescript-tools.nvim',
+  'kokatsu/typescript-tools.nvim',
+  branch = 'feat/tsserver-plugins-location-languages',
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
   ft = {
     'javascript',
     'javascriptreact',
     'typescript',
     'typescriptreact',
+    'vue',
   },
   keys = {
     { '<leader>to', '<cmd>TSToolsOrganizeImports<cr>', desc = 'Organize imports' },
@@ -26,20 +63,30 @@ return {
       'javascriptreact',
       'typescript',
       'typescriptreact',
+      'vue',
     },
-    expose_as_code_action = 'all',
-    complete_function_calls = true,
     single_file_support = false,
-    root_dir = function(fname)
+    settings = {
+      tsserver_path = find_tsserver_path(),
+      expose_as_code_action = 'all',
+      complete_function_calls = true,
+      tsserver_plugins = {
+        {
+          name = '@vue/typescript-plugin',
+          location = find_vue_typescript_plugin(),
+          languages = { 'vue' },
+        },
+      },
+    },
+    root_dir = function(bufnr, on_dir)
       -- Don't start for Deno files (check shebang)
-      local bufnr = vim.fn.bufnr(fname)
-      if bufnr ~= -1 and vim.b[bufnr].is_deno then
-        return nil
+      if vim.b[bufnr].is_deno then
+        return
       end
 
       -- Only start if package.json or tsconfig.json exists
-      local util = require('lspconfig.util')
-      return util.root_pattern('package.json', 'tsconfig.json', 'jsconfig.json')(fname)
+      local root = vim.fs.root(bufnr, { 'package.json', 'tsconfig.json', 'jsconfig.json' })
+      on_dir(root)
     end,
   },
 }
